ec7816843a1b8c1ec4e40ac51abb52709dfc750b
Paul Gierz <pgierz@awi.de>
2020-04-27 17:02:12 +0200

    Bump version: 3.0.0 → 3.1.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_profile',
-    version='3.0.0',
+    version='3.1.0',
     zip_safe=False,
 )
